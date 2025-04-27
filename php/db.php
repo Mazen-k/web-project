eb60ed053003c2e2ebf3ea1cf5fb09dea0b043a4
JamalKassar <Jamal.elkassar@gmail.com>
2025-04-27 11:00:26 +0300

    fixed

--- php/db.php
+++ php/db.php
@@ -1,9 +1,8 @@
 <?php
-/* db.php  –  one place for DB credentials */
 $host = 'localhost';
-$user = 'root';        // change if you set a password
-$pass = '';            //  ↑
-$db   = 'learnpy';     // better than using the system “mysql” DB
+$user = 'root';        
+$pass = '';            
+$db   = 'learnpy';     
 
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
